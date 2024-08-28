ab969a0842f3b651b6d4ef13708a1cf7e612d606
김희선 <rlaltjs15@naver.com>
2024-08-29 00:06:55 +0900

    "2024.08.29"

---
+++ marine-products/src/test/java/com/marine/MarineProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarineProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
