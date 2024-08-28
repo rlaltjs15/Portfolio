91e8c3da90fc7b0798fa6d6ad5465efb08046a18
김희선 <rlaltjs15@naver.com>
2024-08-28 23:40:04 +0900

    "2024.08. 28"

---
+++ gangchun/src/main/java/com/gangchun/GangchunApplication.java
@@ -0,0 +1,13 @@
+package com.gangchun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangchunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangchunApplication.class, args);
+	}
+
+}
---
+++ gangchun/src/test/java/com/gangchun/GangchunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangchun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangchunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
