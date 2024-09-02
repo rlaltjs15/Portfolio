80e7a8c40a197fb87ba181b045a5e91d9af519bd
김희선 <rlaltjs15@naver.com>
2024-09-02 14:19:32 +0900

    2024.09.20

---
+++ eda/src/test/java/com/eda/EdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
